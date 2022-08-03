d583c98a72a5dcb7ea0cb390c0eaf1c8beab92e1
Gabriel Augusto <32905056+gabrielaug@users.noreply.github.com>
2022-08-02 23:22:48 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "5373026462:AAFB8mbYARFCt1L2PCK2bwE_cgRzQn8x8Ew"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "Git Brasil Bot"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/gabrielaug/GitGram"
