cc4d611fe0583484670f96ee705cf985a46979d0
Gabriel Augusto <32905056+gabrielaug@users.noreply.github.com>
2022-08-02 23:47:56 -0300

    Update GitGram.py

--- GitGram.py
+++ GitGram.py
@@ -2,10 +2,11 @@
 
 from logging import basicConfig, getLogger, INFO
 from flask import Flask, request, jsonify
+from html import escape
 from requests import get, post
 from os import environ
 import config
-from markupsafe import escape
+
 from telegram.ext import CommandHandler, Updater
 
 server = Flask(__name__)
